3d33e731b2dc6ee8a397b20e9fc35c1bfd1895ef
fans <jeneryfan@hotmail.com>
2016-10-12 19:14:29 +0800

    统计

--- b2b-daren/daren-dao/src/main/java/com/jumore/b2b/daren/service/IAppraiseDetailService.java
+++ b2b-daren/daren-dao/src/main/java/com/jumore/b2b/daren/service/IAppraiseDetailService.java
@@ -1,10 +1,15 @@
 package com.jumore.b2b.daren.service;
 
+import java.util.Date;
+import java.util.List;
+
 import org.mybatis.generator.base.service.single.IBaseService;
 
 import com.jumore.b2b.daren.model.AppraiseDetail;
 import com.jumore.b2b.daren.model.AppraiseDetailQueryHelper;
 
 public interface IAppraiseDetailService extends IBaseService<AppraiseDetail, AppraiseDetailQueryHelper> {
+
+	List<AppraiseDetail> selectStat(String name, String appraisetype, Date startTime, Date endTime);
 
 }
